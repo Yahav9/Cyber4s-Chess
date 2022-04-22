830864ed1c14f905272089e1855973339bd0e376
Yahav9 <yahav25852@gmail.com>
2022-04-22 21:03:02 +0300

    Mission for Sunday

--- chess.js
+++ chess.js
@@ -1,105 +1,21 @@
 let chessBoard;
 let selectedSquare;
-let pieces = [];
+let boardData;
 
 
-// const WHITE_ROOK = document.createElement('img');
-// WHITE_ROOK.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/45px-Chess_rlt45.svg.png';
-// const WHITE_KNIGHT = document.createElement('img');
-// WHITE_KNIGHT.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/45px-Chess_nlt45.svg.png';
-// const WHITE_BISHOB = document.createElement('img');
-// WHITE_BISHOB.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/45px-Chess_blt45.svg.png';
-// const WHITE_ROOK2 = document.createElement('img');
-// WHITE_ROOK2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/45px-Chess_rlt45.svg.png';
-// const WHITE_KNIGHT2 = document.createElement('img');
-// WHITE_KNIGHT2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/45px-Chess_nlt45.svg.png';
-// const WHITE_BISHOB2 = document.createElement('img');
-// WHITE_BISHOB2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/45px-Chess_blt45.svg.png';
-// const WHITE_QUEEN = document.createElement('img');
-// WHITE_QUEEN.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/45px-Chess_qlt45.svg.png';
-// const WHITE_KING = document.createElement('img');
-// WHITE_KING.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/45px-Chess_klt45.svg.png';
-// const WHITE_PAWN1 = document.createElement('img');
-// WHITE_PAWN1.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN2 = document.createElement('img');
-// WHITE_PAWN2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN3 = document.createElement('img');
-// WHITE_PAWN3.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN4 = document.createElement('img');
-// WHITE_PAWN4.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN5 = document.createElement('img');
-// WHITE_PAWN5.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN6 = document.createElement('img');
-// WHITE_PAWN6.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN7 = document.createElement('img');
-// WHITE_PAWN7.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const WHITE_PAWN8 = document.createElement('img');
-// WHITE_PAWN8.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/45px-Chess_plt45.svg.png';
-// const BLACK_ROOK = document.createElement('img');
-// BLACK_ROOK.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/45px-Chess_rdt45.svg.png';
-// const BLACK_KNIGHT = document.createElement('img');
-// BLACK_KNIGHT.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/45px-Chess_ndt45.svg.png';
-// const BLACK_BISHOB = document.createElement('img');
-// BLACK_BISHOB.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/45px-Chess_bdt45.svg.png';
-// const BLACK_ROOK2 = document.createElement('img');
-// BLACK_ROOK2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/45px-Chess_rdt45.svg.png';
-// const BLACK_KNIGHT2 = document.createElement('img');
-// BLACK_KNIGHT2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/45px-Chess_ndt45.svg.png';
-// const BLACK_BISHOB2 = document.createElement('img');
-// BLACK_BISHOB2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/45px-Chess_bdt45.svg.png';
-// const BLACK_QUEEN = document.createElement('img');
-// BLACK_QUEEN.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/45px-Chess_qdt45.svg.png';
-// const BLACK_KING = document.createElement('img');
-// BLACK_KING.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/45px-Chess_kdt45.svg.png';
-// const BLACK_PAWN1 = document.createElement('img');
-// BLACK_PAWN1.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN2 = document.createElement('img');
-// BLACK_PAWN2.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN3 = document.createElement('img');
-// BLACK_PAWN3.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN4 = document.createElement('img');
-// BLACK_PAWN4.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN5 = document.createElement('img');
-// BLACK_PAWN5.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN6 = document.createElement('img');
-// BLACK_PAWN6.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN7 = document.createElement('img');
-// BLACK_PAWN7.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
-// const BLACK_PAWN8 = document.createElement('img');
-// BLACK_PAWN8.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/45px-Chess_pdt45.svg.png';
+class BoardData {
+  constructor(pieces) {
+    this.pieces = pieces;
+  }
 
-// document.getElementById('1-1').appendChild(WHITE_ROOK);
-// document.getElementById('1-2').appendChild(WHITE_KNIGHT);
-// document.getElementById('1-3').appendChild(WHITE_BISHOB);
-// document.getElementById('1-4').appendChild(WHITE_QUEEN);
-// document.getElementById('1-5').appendChild(WHITE_KING);
-// document.getElementById('1-6').appendChild(WHITE_BISHOB2);
-// document.getElementById('1-7').appendChild(WHITE_KNIGHT2);
-// document.getElementById('1-8').appendChild(WHITE_ROOK2);
-// document.getElementById('2-1').appendChild(WHITE_PAWN1);
-// document.getElementById('2-2').appendChild(WHITE_PAWN2);
-// document.getElementById('2-3').appendChild(WHITE_PAWN3);
-// document.getElementById('2-4').appendChild(WHITE_PAWN4);
-// document.getElementById('2-5').appendChild(WHITE_PAWN5);
-// document.getElementById('2-6').appendChild(WHITE_PAWN6);
-// document.getElementById('2-7').appendChild(WHITE_PAWN7);
-// document.getElementById('2-8').appendChild(WHITE_PAWN8);
-// document.getElementById('8-1').appendChild(BLACK_ROOK);
-// document.getElementById('8-2').appendChild(BLACK_KNIGHT);
-// document.getElementById('8-3').appendChild(BLACK_BISHOB);
-// document.getElementById('8-4').appendChild(BLACK_QUEEN);
-// document.getElementById('8-5').appendChild(BLACK_KING);
-// document.getElementById('8-6').appendChild(BLACK_BISHOB2);
-// document.getElementById('8-7').appendChild(BLACK_KNIGHT2);
-// document.getElementById('8-8').appendChild(BLACK_ROOK2);
-// document.getElementById('7-1').appendChild(BLACK_PAWN1);
-// document.getElementById('7-2').appendChild(BLACK_PAWN2);
-// document.getElementById('7-3').appendChild(BLACK_PAWN3);
-// document.getElementById('7-4').appendChild(BLACK_PAWN4);
-// document.getElementById('7-5').appendChild(BLACK_PAWN5);
-// document.getElementById('7-6').appendChild(BLACK_PAWN6);
-// document.getElementById('7-7').appendChild(BLACK_PAWN7);
-// document.getElementById('7-8').appendChild(BLACK_PAWN8);
+  getPiece(row, col) {
+    for (let piece of this.pieces) {
+      if (piece.row === row && piece.col === col) {
+        return piece;
+      }
+    }
+  }
+}
 
 class Piece {
   constructor(row, col, type, player) {
@@ -108,6 +24,7 @@ class Piece {
     this.type = type;
     this.player = player;
   }
+
   getPossibleMoves() {
     let relativeMoves = [];
     if (this.type === 'pawn') {
@@ -115,13 +32,13 @@ class Piece {
     } else if (this.type === 'rook') {
       relativeMoves = this.getRookRelativeMoves();
     } else if (this.type === 'knight') {
-      relativeMoves = this.getKnightRelativeMoves()
+      relativeMoves = this.getKnightRelativeMoves();
     } else if (this.type === 'bishop') {
-      relativeMoves = this.getBishopRelativeMoves()
+      relativeMoves = this.getBishopRelativeMoves();
     } else if (this.type === 'king') {
-      relativeMoves = this.getKingRelativeMoves()
+      relativeMoves = this.getKingRelativeMoves();
     } else if (this.type === 'queen') {
-      relativeMoves = this.getQueenRelativeMoves()
+      relativeMoves = this.getQueenRelativeMoves();
     }
     let absoluteMoves = [];
     for (let relativeMove of relativeMoves) {
@@ -134,29 +51,31 @@ class Piece {
       const absoluteRow = absoluteMove[0];
       const absoluteCol = absoluteMove[1];
       if (absoluteRow >= 0 && absoluteRow <= 7 && absoluteCol >= 0 && absoluteCol <= 7) {
-        if (!(absoluteRow===this.row && absoluteCol===this.col)){
-        filteredMoves.push(absoluteMove);
-      }}
+        if (!(absoluteRow === this.row && absoluteCol === this.col)) {
+          filteredMoves.push(absoluteMove);
+        }
+      }
     }
     return filteredMoves;
   }
 
+
   //possible moves for each piece
   getPawnRelativeMoves() {
     if (this.row === 1 && this.player === 'black') {
-      return [[1, 0], [2, 0]]
+      return [[1, 0], [2, 0]];
     } else if (this.row !== 1 && this.player === 'black') {
-      return [[1, 0]]
+      return [[1, 0]];
     } else if (this.row === 6 && this.player === 'white') {
-      return [[-1, 0], [-2, 0]]
+      return [[-1, 0], [-2, 0]];
     } else if (this.row !== 6 && this.player === 'white') {
-      return [[-1, 0]]
+      return [[-1, 0]];
     }
   }
 
   getRookRelativeMoves() {
     let moves = [];
-    for (let i = -8; i < 8; i++) {
+    for (let i = -7; i < 8; i++) {
       moves.push([i, 0]);
       moves.push([0, i]);
     }
@@ -167,37 +86,93 @@ class Piece {
     return [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]];
   }
 
-  getBishopRelativeMoves(){
+  getBishopRelativeMoves() {
     let moves = [];
-    for (let i = -8; i < 8; i++) {
+    for (let i = -7; i < 8; i++) {
       moves.push([i, i]);
       moves.push([i, -i]);
     }
     return moves;
   }
-  getKingRelativeMoves(){
-   let moves=[];
-    for (let i=-1; i<2; i++){
-      for (let j=-1; j<2; j++){
-        moves.push([i,j]);
+
+  getKingRelativeMoves() {
+    let moves = [];
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        moves.push([i, j]);
+      }
+    }
+    return moves;
+  }
+
+  getQueenRelativeMoves() {
+    let moves = [];
+    for (let i = -7; i < 8; i++) {
+      moves.push([i, 0]);
+      moves.push([0, i]);
+      moves.push([i, i]);
+      moves.push([i, -i]);
+    }
+    return moves;
+  }
+}
+
+
+// calling the pieces
+function addPieces(result, row, player) {
+  result.push(new Piece(row, 0, 'rook', player));
+  result.push(new Piece(row, 1, 'knight', player));
+  result.push(new Piece(row, 2, 'bishop', player));
+  result.push(new Piece(row, 3, 'queen', player));
+  result.push(new Piece(row, 4, 'king', player));
+  result.push(new Piece(row, 5, 'bishop', player));
+  result.push(new Piece(row, 6, 'knight', player));
+  result.push(new Piece(row, 7, 'rook', player));
+}
+function addPawns(result, row, player) {
+  for (i = 0; i < 8; i++) {
+    result.push(new Piece(row, i, 'pawn', player));
+  }
+}
+
+function getInitialPieces() {
+  let result = [];
+  addPieces(result, 7, 'white');
+  addPawns(result, 6, 'white');
+  addPieces(result, 0, 'black');
+  addPawns(result, 1, 'black');
+  return result
+}
+
+function addImage(square, player, type) {
+  const image = document.createElement('img');
+  image.src = 'images\\' + player + '_' + type + '.png';
+  square.appendChild(image);
+}
+
+
+//click event
+function onSquareClick(event, row, col) {
+  for (let k = 0; k < 8; k++) {
+    for (let l = 0; l < 8; l++) {
+      chessBoard.rows[k].cells[l].classList.remove('possible-move');
     }
   }
-  return moves;
+  const piece = boardData.getPiece(row, col);
+  if (piece !== undefined) {
+    let possibleMoves = piece.getPossibleMoves();
+    for (let possibleMove of possibleMoves)
+      chessBoard.rows[possibleMove[0]].cells[possibleMove[1]].classList.add('possible-move');
+  }
+  if (selectedSquare !== undefined) {
+    selectedSquare.classList.remove('selected');
+  }
+  selectedSquare = event.currentTarget;
+  selectedSquare.classList.add('selected');
 }
 
-getQueenRelativeMoves(){
-  let moves=[];
-  for (let i = -8; i < 8; i++) {
-    moves.push([i, 0]);
-    moves.push([0, i]);
-    moves.push([i, i]);
-    moves.push([i, -i]);
-   }
- return moves;
-}
 
-}
-
+//creating the board and the pieces
 function createChessBoard() {
   chessBoard = document.createElement('table');
   document.body.appendChild(chessBoard);
@@ -215,63 +190,10 @@ function createChessBoard() {
       square.addEventListener('click', (event) => onSquareClick(event, i, j));
     }
   }
-  pieces = getInitialPieces();
-  for (let piece of pieces) {
+  boardData = new BoardData(getInitialPieces());
+  for (let piece of boardData.pieces) {
     addImage(chessBoard.rows[piece.row].cells[piece.col], piece.player, piece.type)
   }
-}
-
-// calling the pieces
-function addPieces(result, row, player) {
-  result.push(new Piece(row, 0, 'rook', player));
-  result.push(new Piece(row, 1, 'knight', player));
-  result.push(new Piece(row, 2, 'bishop', player));
-  result.push(new Piece(row, 3, 'queen', player));
-  result.push(new Piece(row, 4, 'king', player));
-  result.push(new Piece(row, 5, 'bishop', player));
-  result.push(new Piece(row, 6, 'knight', player));
-  result.push(new Piece(row, 7, 'rook', player));
-}
-function addPawns(result, row, player) {
-  for (i = 0; i < 8; i++) {
-    result.push(new Piece(row, i, 'pawn', player));
-  }
-};
-function getInitialPieces() {
-  let result = [];
-  addPieces(result, 7, 'white');
-  addPawns(result, 6, 'white');
-  addPieces(result, 0, 'black');
-  addPawns(result, 1, 'black');
-  return result
-}
-function addImage(square, player, type) {
-  const image = document.createElement('img');
-  image.src = 'images\\' + player + '_' + type + '.png';
-  square.appendChild(image);
-}
-
-
-
-
-function onSquareClick(event, row, col) {
-  for (let k = 0; k < 8; k++) {
-    for (let l = 0; l < 8; l++) {
-      chessBoard.rows[k].cells[l].classList.remove('possible-move');
-    }
-  }
-  for (let piece of pieces) {
-    if (piece.row === row && piece.col === col) {
-      let possibleMoves = piece.getPossibleMoves();
-      for (let possibleMove of possibleMoves)
-        chessBoard.rows[possibleMove[0]].cells[possibleMove[1]].classList.add('possible-move');
-    }
-  }
-  if (selectedSquare !== undefined) {
-    selectedSquare.classList.remove('selected');
-  }
-  selectedSquare = event.currentTarget;
-  selectedSquare.classList.add('selected');
 }
 
 
